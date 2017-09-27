e3ef47213967c39e987c2a8fc9010109f173d0d4
Koen Hufkens <khufkens@Pandora-2.local>
2017-09-27 21:02:20 +0200

    CRAN check

--- R/calculate_leaf_traits.r
+++ R/calculate_leaf_traits.r
@@ -1,15 +1,15 @@
 #' Calculates leaf traits for a directory of leaf scans
 #' either made with a white or black background.
 #'
-#' @param path: path of image files to process (should only contain images),
+#' @param path path of image files to process (should only contain images),
 #' default is your home directory
-#' @param dpi: dots per inch resolution of the scan (default = 300)
-#' @param background: background matte used ("white" or "black")
-#' @param reference: is there a reference strip to the left which
+#' @param dpi dots per inch resolution of the scan (default = 300)
+#' @param background background matte used ("white" or "black")
+#' @param reference is there a reference strip to the left which
 #' should be excluded, provide a number of pixels to do so (default = NULL)
-#' @param out_path: output directory where to save the data if not returned
+#' @param out_path output directory where to save the data if not returned
 #' to an R variable
-#' @param plot: plot the processed images for reference (default = FALSE)
+#' @param plot plot the processed images for reference (default = FALSE)
 #' @return A nested list (data structure) listing the extracting traits together
 #' with the filename of the processed file.
 #' @keywords leaf traits, specific leaf area, leaf area, Gcc, Rcc
@@ -36,20 +36,20 @@ calculate_leaf_traits = function(path = "~",
     m[m == 0] = NA
 
     # read image bands
-    r = raster(img, band=1)
-    g = raster(img, band=2)
-    b = raster(img, band=3)
+    r = raster::raster(img, band=1)
+    g = raster::raster(img, band=2)
+    b = raster::raster(img, band=3)
 
     # mask all values which are of no
     # importance
-    r_s = mask(r, m, maskevalue = NA)
-    g_s = mask(g, m, updatevalue = NA)
-    b_s = mask(b, m, updatevalue = NA)
+    r_s = raster::mask(r, m, maskevalue = NA)
+    g_s = raster::mask(g, m, updatevalue = NA)
+    b_s = raster::mask(b, m, updatevalue = NA)
 
     # average across the ROI
-    r_m = cellStats(r_s, stat = mean)
-    g_m = cellStats(g_s, stat = mean)
-    b_m = cellStats(b_s, stat = mean)
+    r_m = raster::cellStats(r_s, stat = mean)
+    g_m = raster::cellStats(g_s, stat = mean)
+    b_m = raster::cellStats(b_s, stat = mean)
 
     # calculate the gcc (% green)
     gcc =  g_m/(r_m + g_m + b_m)
@@ -70,16 +70,19 @@ calculate_leaf_traits = function(path = "~",
 
   batch_output = lapply(files,function(file){
 
+    # feedback
+    cat(sprintf("processing image: %s \n", file))
+
     # read in image
-    im = load.image(file)
+    im = imager::load.image(file)
 
     if (!is.null(reference)){
-      px = Xc(im) <= reference
+      px = imager::Xc(im) <= reference
       # blank out reference panels
       # could be done manually as well
       if (background == "white"){
-        bg = imfill(dim=dim(im),val=c(1,1,1))
-        msk = as.cimg(px)
+        bg = imager::imfill(dim=dim(im),val=c(1,1,1))
+        msk = imager::as.cimg(px)
         im = bg*msk+(1-msk)*im
       }else{
         im[px] = 0
@@ -88,24 +91,26 @@ calculate_leaf_traits = function(path = "~",
 
     # convert to CIELAB colour space,
     # then create a data.frame with three colour channels as columns
-    d = sRGBtoLab(im) %>% as.data.frame(wide="c") %>% dplyr::select(-x,-y)
-    L = channel(RGBtoHSL(im),3)
+    d = imager::sRGBtoLab(im)
+    d = as.data.frame(d, wide="c")[,3:5]
+    L = imager::channel(imager::RGBtoHSL(im),3)
+
     # convert to raster format
-    L = raster(t(as.matrix(L)))
-    extent(L) = extent(0,ncol(L),0,nrow(L))
+    L = raster::raster(t(as.matrix(L)))
+    raster::extent(L) = raster::extent(0,ncol(L),0,nrow(L))
 
     # run k-means with 2 centers
     km = kmeans(d,2)
 
     # turn cluster index into an image
-    seg = as.cimg(km$cluster,dim=c(dim(im)[1:2],1,1))
+    seg = imager::as.cimg(km$cluster,dim=c(dim(im)[1:2],1,1))
 
     # convert to raster format
-    seg = raster(t(as.matrix(seg)))
-    extent(seg) = extent(0,ncol(seg),0,nrow(seg))
+    seg = raster::raster(t(as.matrix(seg)))
+    raster::extent(seg) = raster::extent(0,ncol(seg),0,nrow(seg))
 
     # calculate zonal statistics
-    zstats = as.data.frame(zonal(L, seg, fun = "mean", na.rm = TRUE))
+    zstats = as.data.frame(raster::zonal(L, seg, fun = "mean", na.rm = TRUE))
 
     if (background == "white"){
       seg[seg == which(zstats$mean == max(zstats$mean, na.rm = TRUE))] = NA
@@ -115,25 +120,24 @@ calculate_leaf_traits = function(path = "~",
 
     # clump the data
     # kick out anything smaller than 100 px
-    c = clump(seg, gaps = FALSE)
-    extent(c) = extent(0,ncol(c),0,nrow(c))
-    for(i in which(freq(c)[,2] < 100) ){
+    c = raster::clump(seg, gaps = FALSE)
+    raster::extent(c) = raster::extent(0,ncol(c),0,nrow(c))
+    for(i in which(raster::freq(c)[,2] < 100) ){
      c[c == i] = NA
     }
 
     # get centroids
-    clump_id = getValues(c)
-    xy = xyFromCell(c,1:ncell(c))
+    clump_id = raster::getValues(c)
+    xy = raster::xyFromCell(c,1:raster::ncell(c))
     df = na.omit(data.frame(xy, clump_id))
     centroids = by(df,INDICES = df$clump_id, function(xy){
-      x = median(xy$x[1])
-      y = median(xy$y[2])
-      data.frame(x,y)}
-       )
+      data.frame(median(xy$x[1]),
+                 median(xy$y[2]))
+      })
     centroids = do.call("rbind",centroids)
 
     # discard noise
-    pixel_counts = as.data.frame(freq(c, useNA = 'no'))
+    pixel_counts = as.data.frame(raster::freq(c, useNA = 'no'))
     results = lapply(pixel_counts$value,function(updatevalue){
                 getCover(file, c, updatevalue)
               })
@@ -150,7 +154,7 @@ calculate_leaf_traits = function(path = "~",
 
     # plot results for feedback
     if (plot){
-      plotRGB(brick(file))
+      raster::plotRGB(raster::brick(file))
       text(pixel_stats$x,
            pixel_stats$y,
            adj = c(0,3),
